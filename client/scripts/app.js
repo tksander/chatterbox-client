96b6ebf25a3e3e43e26c2a21d82dbbb1961960a1
tksander <tkepasander@gmail.com>
2015-08-31 17:24:32 -0700

    Sanitizer implemented

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,54 @@
 // YOUR CODE HERE:
 var safeInput = function(input){
-  return encodeURI(input);
+  //var wacky = "<script>alert('a')</script>"
+ // return encodeURI(input);
+
+ //, <, >, ", ', `, , !, @, $, %, (, ), =, +, {, }, [, and ] 
+ //Ref: http://www.ascii.cl/htmlcodes.htm
+ //Ref: http://wonko.com/post/html-escaping
+
+  var sanitizedInput = [];
+  var badContent = false;
+
+  if (input){
+    for (var i = 0; i < input.length; i++){
+      var currentChar = input[i];
+
+      if (currentChar === " "){
+        sanitizedInput[i] = "&#32;";
+        badContent = true;
+      }
+
+      if (currentChar === "<"){
+        sanitizedInput[i] = "&#60;";
+        badContent = true;
+      }
+
+     if (currentChar === ">"){
+        sanitizedInput[i] = "&#62;";
+        badContent = true;
+      }
+
+      if (currentChar === "$"){
+        sanitizedInput[i] = "&#36;";
+        badContent = true;
+      }
+
+      if (!badContent){
+        sanitizedInput[i] = input[i];             
+      }
+
+      badContent = false;
+    }
+
+  }
+
+  return sanitizedInput.join("");
+
+
 };
+
+
 
 var drawChats = function(messageObj){
 
@@ -9,13 +56,15 @@ var drawChats = function(messageObj){
   template += '<div class="text" data-text="'  + safeInput(messageObj.text) +'">' + safeInput(messageObj.text) + '</div>';
   template += '<div class="username" data-username="' +  safeInput(messageObj.username) + '">' + safeInput(messageObj.username) + '</div>'; 
   template += '<div class="room>' + safeInput(messageObj.roomname) + '</div>';
+  template += '<div class="timeCreated">' + messageObj.createdAt + '</div>'
   template += '</div>'
-  $('#chats').append(template);
+  $('#chats').prepend(template);
 };
 
 
 var app = {
-
+  skip: 0,
+  limit: 1000,
   server: "https://api.parse.com/1/classes/chatterbox",
   friends: [],
 
@@ -50,11 +99,16 @@ var app = {
 },
 
 fetch: function(){
+  //#Bugfix: Clicking button multiple times results in repeats
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
+    data : {
+    limit : app.limit,
+    order : "-createdAt",
+    },
     success: function (data) {
       console.log('Got data');
       console.log(data);
@@ -62,10 +116,11 @@ fetch: function(){
       for (var i = 0; i < data.results.length; i++){
          drawChats(data.results[i]);
       }
+      app.skip += 10;
     },
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to retrieve messages');
     }
   });
   },
@@ -107,13 +162,8 @@ fetch: function(){
 
 $(document).ready(function(){
   //Run the app
-  app.addMessage({
-  username: 'Mel Brooks',
-  text: 'I didn\'t get a harumph outa that guy.!',
-  roomname: 'lobby'
-});
 
-app.init();
+  app.init();
 
 
   $("#clearMessagesButton").on("click", function(){
@@ -160,4 +210,6 @@ app.init();
     app.handleSubmit();
   })
 
+  //Prepend the new stuff
+  //append the old stuff
 });
